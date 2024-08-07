01183b88c95a7eceee175fc7cfe2b7dda14ab94a
SaraSaber <saraasaber77@gmail.com>
2024-08-07 23:29:00 +0430

    feat(bottom_navigation):set bottom navigation

--- app/src/main/java/ir/developer/todolist/MainActivity.kt
+++ app/src/main/java/ir/developer/todolist/MainActivity.kt
@@ -3,18 +3,25 @@ package ir.developer.todolist
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import ir.developer.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
+//        window.navigationBarColor = ContextCompat.getColor(this, R.color.Watery)
+//        window.statusBarColor = ContextCompat.getColor(this, R.color.friendlyFrost)
+
+        navController = findNavController(R.id.my_nav_host_fragment)
     }
 }
--- app/src/main/java/ir/developer/todolist/database/AppDataBase.kt
+++ app/src/main/java/ir/developer/todolist/database/AppDataBase.kt
@@ -1,44 +1,44 @@
-package ir.developer.todolist.database
-
-import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
-
-@Database(
-    entities = [],
-    version = 1,
-    exportSchema = true
-)
-
-abstract class AppDataBase : RoomDatabase() {
-
-    companion object {
-        @Volatile
-        private var INSTANCE: AppDataBase? = null
-
-        fun getDatabase(context: Context): AppDataBase {
-            // if the INSTANCE is not null, then return it,
-            // if it is, then create the database
-            if (INSTANCE == null) {
-                synchronized(this) {
-                    // Pass the database to the INSTANCE
-                    INSTANCE = buildDatabase(context)
-                }
-            }
-            // Return database.
-            return INSTANCE!!
-        }
-
-        private fun buildDatabase(context: Context): AppDataBase {
-            return Room.databaseBuilder(
-                context.applicationContext,
-                AppDataBase::class.java,
-                "game_database"
-            )
-                .allowMainThreadQueries()
-                .fallbackToDestructiveMigration()
-                .build()
-        }
-    }
-}
+//package ir.developer.todolist.database
+//
+//import android.content.Context
+//import androidx.room.Database
+//import androidx.room.Room
+//import androidx.room.RoomDatabase
+//
+//@Database(
+//    entities = [],
+//    version = 1,
+//    exportSchema = true
+//)
+//
+//abstract class AppDataBase : RoomDatabase() {
+//
+//    companion object {
+//        @Volatile
+//        private var INSTANCE: AppDataBase? = null
+//
+//        fun getDatabase(context: Context): AppDataBase {
+//            // if the INSTANCE is not null, then return it,
+//            // if it is, then create the database
+//            if (INSTANCE == null) {
+//                synchronized(this) {
+//                    // Pass the database to the INSTANCE
+//                    INSTANCE = buildDatabase(context)
+//                }
+//            }
+//            // Return database.
+//            return INSTANCE!!
+//        }
+//
+//        private fun buildDatabase(context: Context): AppDataBase {
+//            return Room.databaseBuilder(
+//                context.applicationContext,
+//                AppDataBase::class.java,
+//                "game_database"
+//            )
+//                .allowMainThreadQueries()
+//                .fallbackToDestructiveMigration()
+//                .build()
+//        }
+//    }
+//}
